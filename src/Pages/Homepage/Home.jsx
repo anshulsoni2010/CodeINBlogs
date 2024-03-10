d0537da5d2562d8a28dd59a218adb0c91879ada2
Anshul Soni <anshulsoni2022@gmail.com>
2024-03-10 12:50:35 +0530

    benefits section completed

--- src/Pages/Homepage/Home.jsx
+++ src/Pages/Homepage/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ParnterSlider from './components/parnter-slider';
 import Stats from './components/stats.jsx'
+import Footer from './components/footer.jsx';
+import BenefitsCards from './components/benefits.jsx';
 const HomePage = () => {
     return (
 <>
         <ParnterSlider></ParnterSlider>
         <Stats></Stats>
+        <BenefitsCards></BenefitsCards>
+        <Footer></Footer>
+
         </>
     );
 };
---
+++ src/Pages/Homepage/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "./image/download.png"
+const Footer = () => {
+
+
+    return (
+        <>
+
+
+
+            
+        </>
+    );
+}
+
+export default Footer
