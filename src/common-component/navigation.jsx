33ddb3cf9218e233f1eb948107084b5d15ed1f38
Anshul Soni <anshulsoni2022@gmail.com>
2024-03-10 15:38:42 +0530

    update text color changed to gradient

--- src/common-component/navigation.jsx
+++ src/common-component/navigation.jsx
@@ -1,36 +1,49 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 import "../styles/navigation.css";
 
-
 const Navigation = () => {
-    const navref=useRef();
+    const navRef = useRef();
 
-    const showNav=()=>{
-        navref.current.classList.toggle('responsive-nav')
-    }
-  return (
-    <div className='nav'>
-        <div className="nav-logo">
-            <img src="logo.png" alt="" />
-        </div>
-        <div className="nav-elements" ref={navref}>
-            <span>Home</span>
-            <span>Blogs</span>
-            <span>Events</span>
-            <span>About Us</span>
-            <span>Resources</span>
-            <span>Products</span>
-        </div>
+    const handleMouseEnter = (event) => {
+        const underline = document.createElement('div');
+        underline.classList.add('underline');
+        underline.style.backgroundColor = getComputedStyle(event.target).getPropertyValue('color');
+        event.target.appendChild(underline);
+    };
 
-  
-        <div className="btn">
-            <button>Sign Up</button>
-        </div>
-        <div>
-        <img src="burger.svg" className='nav-btn  nav-close' onClick={showNav} width={50}/>
-    </div>
-    </div>
-  )
-}
+    const handleMouseLeave = (event) => {
+        const underline = event.target.querySelector('.underline');
+        if (underline) {
+            event.target.removeChild(underline);
+        }
+    };
 
-export default Navigation
+    const showNav = () => {
+        navRef.current.classList.toggle('responsive-nav');
+    };
+
+    return (
+        <div className='nav'>
+            <div className="nav-logo">
+                <img src="logo.png" alt="" />
+            </div>
+            <div className="nav-elements" ref={navRef}>
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Home</span>
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Blogs</span>
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Events</span>
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>About Us</span>
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Resources</span>
+                <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Products</span>
+            </div>
+
+            <div className="btn">
+                <button>Sign Up</button>
+            </div>
+            <div>
+                <img src="burger.svg" className='nav-btn  nav-close' onClick={showNav} width={50} />
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
