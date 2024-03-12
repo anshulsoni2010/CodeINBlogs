e56d8a4237688764fe710fe2d90a4fa2f4c9d2a2
simarjot <simarjot0032@gmail.com>
2024-03-12 21:42:57 +0530

    added hero section

---
+++ src/Pages/Homepage/components/Herosection.jsx
@@ -0,0 +1,36 @@
+export default function Herosection() {
+  return (
+    <>
+      <div className="hero-section-container">
+        <div className="hero-content">
+          <div className="hero-section-content">
+            <div className="hero-section-join-community-message">
+              Join our community of 15,000+ developers now!
+            </div>
+            <div className="hero-section-powerful-message">
+              Powerful Platform & Community For
+            </div>
+            <h1 className="hero-section-heading-for-developers">DEVELOPERS</h1>
+          </div>
+          <div className="hero-section-para-container">
+            <p className="hero-section-para">
+              {" "}
+              Your hub for innovation, collaboration, and endless possibilities!
+              Ignite your passion through insightful blogs, connect with a
+              vibrant tech community, and elevate your skills in thrilling
+              hackathons. Join us, where coding is limitless, and the excitement
+              never fades!{" "}
+            </p>
+          </div>
+
+          <div className="hero-section-btns">
+            <div className="get-started-container">
+              <button className="hero-section-get-started">Get Started</button>
+            </div>
+            <button className="hero-section-explore-now">Explore Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
